0257e12629a7ad92d77a4846c8df6d44c639d3e1
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-05-28 14:11:19 +0530

    x

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () =>{
     <>
       <Nav/>
       <Header/> 
-      <About description='A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I am still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.Besides coding, I love traveling and meeting new people, playing Cricket or any online game .'  image='./components/assets/imgs/man.png'/> 
+      <About description='A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I am still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.Besides coding, I love traveling and meeting new people, playing Cricket or any online game .'  image='C:\Users\Owais\OneDrive\Desktop\Own_project\portfolio\src\assets\imgs\man.png'/> 
       < Services/>
       <Portfolio/>
       <Price></Price>
