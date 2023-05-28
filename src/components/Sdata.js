072093a57c1342758d07dcd44e7b83acc1898afc
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-05-28 20:30:30 +0530

    A

--- src/components/Sdata.js
+++ src/components/Sdata.js
@@ -1,7 +1,7 @@
 const Sdata = [
 {
     description:'A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I am still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.Besides coding, I love traveling and meeting new people, playing Cricket or any online game .',            
-    image:"C:\Users\Owais\OneDrive\Desktop\Own_project\portfolio\src\assets\imgs\man.png"
+    image:"file:///C:/Users/Owais/OneDrive/Desktop/Own_project/portfolio/src/assets/imgs/man.png"
 
 
 
