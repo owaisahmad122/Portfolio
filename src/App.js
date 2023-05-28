3d257572b1e96815f4b22da38ebcf4c4f3996ea0
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-05-28 19:27:41 +0530

    c

--- src/App.js
+++ src/App.js
@@ -16,24 +16,14 @@ import Sdata from './components/Sdata'
 
 
 //const Ssdata =Sdata.map(val) 
-const Ssdata = [
-  {
-      description:'A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I am still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.Besides coding, I love traveling and meeting new people, playing Cricket or any online game .',            
-      image:'../assets/imgs/man.png'
-  
-  
-  
-  
-  } 
-]
 
-//console.log(Sdata);
+console.log(Sdata.description);
 const App = () =>{
   return (
     <>
       <Nav/>
       <Header/> 
-      <About description='A passionate Full Web Developer having experience of building web application with JavaScript/Reactjs/Nodejs/ Reduxjs and some other cool libraries and frameworks.I am still enthusiastically learning more programming languages, frameworks, or principles I can integrate into the coding web in my head.Besides coding, I love traveling and meeting new people, playing Cricket or any online game .'  image='C:\Users\Owais\OneDrive\Desktop\Own_project\portfolio\src\assets\imgs\man.png'/> 
+      <About description={} image='C:\Users\Owais\OneDrive\Desktop\Own_project\portfolio\src\assets\imgs\man.png'/> 
       < Services/>
       <Portfolio/>
       <Price></Price>
