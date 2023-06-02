bff8db1f95cf3b6b2415065d15d0777959d6e9a2
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-06-03 03:20:22 +0530

    a

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 
 const Services =(data) => {
-    console.log(data)
+    console.log(`data.iamge`)
   return (
     <section className="section" id="service">
     <div className="container text-center">
