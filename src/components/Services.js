986e0bf34bd5b14ebeb0dd57de0761f5045b3368
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-06-03 02:45:04 +0530

    a

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 
 const Services =({data}) => {
-    console.log(data)
+    console.log(data.image)
   return (
     <section className="section" id="service">
     <div className="container text-center">
