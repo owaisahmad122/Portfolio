8efc53ddb49906667c779f00bea73823ec320e96
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-06-02 12:21:37 +0530

    x

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,8 @@
 import React from 'react'
 //import Man from '../assets/imgs/man.png'
 
-function About(props) {
-   console.log(props);
+const About = ({data}) => {
+   console.log(data.image);
     
   return (
     <section className="section pt-0" id="about">
@@ -11,13 +11,13 @@ function About(props) {
        
         <div className="about">
             <div className="about-img-holder">
-                <img src={props.image} className="about-img" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"/>
+                <img src={data.image} className="about-img" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"/>
             </div>
             <div className="about-caption">
                 <p className="section-subtitle">Who Am I ?</p>
                     <h2 className="section-title mb-3">About Me</h2>
                 <p>   
-                 {props.description}
+                 {data.description}
                  </p>
                 <button className="btn-rounded btn btn-outline-primary mt-4">{' '}Download CV</button>
             </div>              
