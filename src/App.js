8873ebaa57557a13c4eb1b219fdd3ba0cec61479
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-06-03 19:01:10 +0530

    x

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import Sdata from './components/Sdata';
 //<Portfolio/>
 //<Price></Price>
 const App = () =>{
-  console.log(Sdata[1,3]);
+  console.log(Sdata.slice[1,3]);
   let SArr ;
    //Sarr=Sdata.slice[2:3]
   const MapComponent ={
