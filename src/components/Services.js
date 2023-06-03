d35c85cda04704176e972fb9809ff1bb76fd245a
owaisahmad122 <55344350+owaisahmad122@users.noreply.github.com>
2023-06-03 13:01:26 +0530

    x

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,7 +6,7 @@ import React from 'react'
 
 
 const Services =(data,index) => {
-    console.log(data.index[1])
+    console.log(index)
   return (
     <section className="section" id="service">
     <div className="container text-center">
